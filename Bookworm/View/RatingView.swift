1b4d0931c12554f9b00bac6d4130f4964b847bf9
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-11 12:02:38 +0530

    rating complete

---
+++ Bookworm/View/RatingView.swift
@@ -0,0 +1,55 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by PRABALJIT WALIA     on 11/11/20.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    
+    @Binding var rating:Int
+    
+    var label = ""
+
+    var maximumRating = 5
+
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    
+    var body: some View {
+        HStack{
+            if label.isEmpty == false{
+                Text(label)
+            }
+            ForEach(1..<maximumRating){number in
+                self.image(for: number)
+                    .foregroundColor(number>self.rating ? self.offColor:self.onColor)
+                    .onTapGesture{
+                        self.rating = number
+                    }
+            }
+        }
+    }
+    func image(for number:Int)-> Image{
+        if number > rating{
+            return offImage ?? onImage
+        }
+        else{
+            return onImage
+        }
+    }
+}
+
+
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
